f3e5b521076184230d808b850156b82e397a2645
Munib28 <101625705+Munib28@users.noreply.github.com>
2023-07-03 16:48:36 +0200

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { NavLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import AuthProviders from "./AuthProviders";
+
+const Navbar = () => {
+  const session = {};
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10 ">
+        <Link href="/">
+          <Image
+            src="/logo2.png"
+            width={60}
+            height={60}
+            alt="Find Talent"
+          />
+        </Link>
+        <ul className="xl:flex hidden text-sm gap-7">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            UserPhoto
+
+            <Link href="/create-project" >
+              Share Your Work
+            </Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
